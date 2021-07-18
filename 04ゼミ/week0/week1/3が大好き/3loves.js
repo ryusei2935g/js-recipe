d6f9e189442dfaacf0517391ca0de3518b0b5558
ryusei.yasuda <ryusei2935g@outlook.jp>
2021-07-18 21:07:26 +0900

    ワーク3-1終わりました

---
+++ 04ゼミ/week0/week1/3が大好き/3loves.js
@@ -0,0 +1,10 @@
+const genkiFunction = function(number) {
+  for (let n = 1; n <= 100; n++) {
+    String(n)
+    if (n === "3") {
+      console.log(`${n}大好き`)
+    } else {
+      console.log(n)
+    }
+  }
+}
